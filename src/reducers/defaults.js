1ea1073b65508e051e6aecdf241661e3a04a6f42
Alex Suraci <suraci.alex@gmail.com>
2016-07-06 15:31:26 -0700

    new team

--- src/reducers/defaults.js
+++ src/reducers/defaults.js
@@ -3,38 +3,36 @@
 
 export const defaultState = {
   assignments: {
-    "0": ["kris", "maria"],
-    "1": ["cdutra", "yucheng"],
-    "2": ["jake", "evan"],
-    "3": ["kim", "alex"],
+    "0": ["mariash"],
+    "1": ["cdutra", "jraqula"],
+    "2": ["eshort", "Pirolf"],
+    "3": ["kimeberz", "vito"],
     "4": [],
     "out": [],
-    "unassigned": ["caleb"],
+    "unassigned": [],
     "pm": ["alejandro"]
   },
   tracks: [
     "0", "1", "2", "3", "4",
   ],
   trackNames: {
-    "0": "Teams",
-    "1": "Binaries",
+    "0": "Backlog",
+    "1": "Backlog",
     "2": "Backlog",
     "3": "Design",
     "4": "Solo"
   },
   photos: {
-    "alex": "https://avatars0.githubusercontent.com/u/1880?v=3&s=72",
-    "kris": "https://avatars0.githubusercontent.com/u/79357?v=3&s=72",
-    "maria": "https://avatars1.githubusercontent.com/u/52845?v=3&s=72",
-    "kim": "https://avatars0.githubusercontent.com/u/2119227?v=3&s=72",
-    "caleb": "https://avatars3.githubusercontent.com/u/848394?v=3&s=72",
-    "jake": "https://avatars0.githubusercontent.com/u/13525256?v=3&s=72",
-    "evan": "https://avatars2.githubusercontent.com/u/7635837?v=3&s=72",
+    "vito": "https://avatars0.githubusercontent.com/u/1880?v=3&s=72",
+    "mariash": "https://avatars1.githubusercontent.com/u/52845?v=3&s=72",
+    "kimeberz": "https://avatars0.githubusercontent.com/u/2119227?v=3&s=72",
+    "jraqula": "https://avatars2.githubusercontent.com/u/4714857?v=3&s=72",
+    "eshort": "https://avatars2.githubusercontent.com/u/7635837?v=3&s=72",
     "cdutra": "https://avatars2.githubusercontent.com/u/5061344?v=3&s=72",
-    "yucheng": "https://avatars1.githubusercontent.com/u/5309486?v=3&s=72",
+    "Pirolf": "https://avatars1.githubusercontent.com/u/5309486?v=3&s=72",
     "alejandro": "https://avatars3.githubusercontent.com/u/17837681?v=3&s=72"
   },
   badges: { "1": [ "CI" ] },
-  locked: { "kris": true, "kim": true, "alex": true },
+  locked: { "kimeberz": true, "vito": true },
   version: 0,
 }
