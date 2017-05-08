e26a61f3f4b7724b6ee0339d6fa67496c65a8a19
Alex Suraci <asuraci@pivotal.io>
2017-05-08 13:22:25 -0400

    convert josh to andrew
    Signed-off-by: Clara Fu <cfu@pivotal.io>

--- src/reducers/defaults.js
+++ src/reducers/defaults.js
@@ -4,7 +4,7 @@ export const defaultState = {
     "1": [],
     "2": [],
     "out": [],
-    "unassigned": ["topher", "clara", "alex", "chris", "josh", "maria"],
+    "unassigned": ["topher", "clara", "alex", "chris", "andrew", "maria"],
     "pm": ["james", "lindsay"]
   },
   "tracks": [
@@ -23,7 +23,7 @@ export const defaultState = {
     "james": "https://avatars3.githubusercontent.com/u/19274230?v=3&s=80",
     "lindsay": "https://ca.slack-edge.com/T07RXRUSU-U4Q61BMFC-g3a93dd13dd1-80",
     "chris": "https://avatars3.githubusercontent.com/u/474000?v=3&s=80",
-    "josh": "https://avatars0.githubusercontent.com/u/3689026?v=3&s=80",
+    "andrew": "https://avatars2.githubusercontent.com/u/10427453?v=3&s=80",
     "clara": "https://avatars3.githubusercontent.com/u/19290703?v=3&s=80",
     "topher": "https://avatars3.githubusercontent.com/u/1895900?v=3&s=80"
   },
