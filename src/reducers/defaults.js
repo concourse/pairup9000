408c4f50275307a08f0bb7fde3aa4522f3474e8f
Alex Suraci <suraci.alex@gmail.com>
2017-09-18 10:38:22 -0400

    update pairs, add track

--- src/reducers/defaults.js
+++ src/reducers/defaults.js
@@ -3,39 +3,35 @@ export const defaultState = {
     "0": [],
     "1": [],
     "2": [],
+    "3": [],
     "out": [],
-    "unassigned": ["topher", "clara", "alex", "chris", "mark", "jtarchie", "gabriel", "joris"],
+    "unassigned": ["topher", "tanzeeb", "alex", "mark", "jtarchie", "gabriel", "joris"],
     "pm": ["james", "lindsay"]
   },
   "tracks": [
     "0",
     "1",
-    "2"
+    "2",
+    "3"
   ],
   "trackNames": {
     "0": "A",
     "1": "B",
-    "2": "C"
+    "2": "C",
+    "3": "D"
   },
   "photos": {
     "alex": "https://avatars0.githubusercontent.com/u/1880?v=3&s=80",
     "mark": "https://avatars1.githubusercontent.com/u/1919762?v=3&s=80",
     "james": "https://avatars3.githubusercontent.com/u/19274230?v=3&s=80",
     "lindsay": "https://ca.slack-edge.com/T07RXRUSU-U4Q61BMFC-g3a93dd13dd1-80",
-    "chris": "https://avatars3.githubusercontent.com/u/474000?v=3&s=80",
-    "andrew": "https://avatars2.githubusercontent.com/u/10427453?v=3&s=80",
-    "clara": "https://avatars3.githubusercontent.com/u/19290703?v=3&s=80",
+    "tanzeeb": "https://avatars3.githubusercontent.com/u/8570?v=3&s=80",
     "topher": "https://avatars3.githubusercontent.com/u/1895900?v=3&s=80",
-    "ergin": "https://avatars3.githubusercontent.com/u/2236882?v=3&s=80",
     "jtarchie": "https://avatars3.githubusercontent.com/u/75184?v=3&s=80",
     "gabriel": "https://avatars3.githubusercontent.com/u/9042962?v=3&s=80",
     "joris": "https://avatars3.githubusercontent.com/u/687348?v=3&s=80"
   },
-  "badges": {
-    "3": [
-      "CI"
-    ]
-  },
+  "badges": {},
   "locked": {},
   "version": 16
 }
