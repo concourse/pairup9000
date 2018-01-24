1c1ce60062161c416aa7f54f6fa4a1132256310e
Mark Huang <mhuang@pivotal.io>
2018-01-24 09:51:34 -0500

    Remove tanzeeb, jt, gabriel
    Signed-off-by: Mark Huang <mhuang@pivotal.io>

--- src/reducers/defaults.js
+++ src/reducers/defaults.js
@@ -6,7 +6,7 @@ export const defaultState = {
     "3": [],
     "4": [],
     "out": [],
-    "unassigned": ["topher", "tanzeeb", "alex", "mark", "jtarchie", "gabriel", "joris", "rui", "josh"],
+    "unassigned": ["topher", "alex", "mark", "joris", "rui", "josh"],
     "pm": ["james", "lindsay"]
   },
   "tracks": [
@@ -28,10 +28,7 @@ export const defaultState = {
     "mark": "https://avatars1.githubusercontent.com/u/1919762?v=3&s=80",
     "james": "https://avatars3.githubusercontent.com/u/19274230?v=3&s=80",
     "lindsay": "https://ca.slack-edge.com/T07RXRUSU-U4Q61BMFC-g3a93dd13dd1-80",
-    "tanzeeb": "https://avatars3.githubusercontent.com/u/8570?v=3&s=80",
     "topher": "https://avatars3.githubusercontent.com/u/1895900?v=3&s=80",
-    "jtarchie": "https://avatars3.githubusercontent.com/u/75184?v=3&s=80",
-    "gabriel": "https://avatars3.githubusercontent.com/u/9042962?v=3&s=80",
     "joris": "https://avatars3.githubusercontent.com/u/687348?v=3&s=80",
     "rui" : "https://avatars2.githubusercontent.com/u/1585949?v=4&s=80",
     "josh" : "https://avatars3.githubusercontent.com/u/1072233?v=4&s=80"
