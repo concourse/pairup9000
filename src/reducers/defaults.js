1b6ec9353e39db5db93b28a41b2b1d30c549da06
Joshua Winters <jwinters@pivotal.io>
2018-06-04 09:42:14 -0400

    Add Divya, Remove Shash, Joris

--- src/reducers/defaults.js
+++ src/reducers/defaults.js
@@ -6,7 +6,7 @@ export const defaultState = {
     "3": [],
     "4": [],
     "out": [],
-    "unassigned": ["topher", "mark", "joris", "rui", "josh", "shash", "nader", "jamie"],
+    "unassigned": ["topher", "mark", "rui", "josh", "nader", "jamie", "divya"],
     "pm": ["alex", "james", "lindsay"]
   },
   "tracks": [
@@ -29,12 +29,11 @@ export const defaultState = {
     "james": "https://avatars3.githubusercontent.com/u/19274230?v=3&s=80",
     "lindsay": "https://ca.slack-edge.com/T07RXRUSU-U4Q61BMFC-g3a93dd13dd1-80",
     "topher": "https://avatars3.githubusercontent.com/u/1895900?v=3&s=80",
-    "joris": "https://avatars3.githubusercontent.com/u/687348?v=3&s=80",
     "rui" : "https://avatars2.githubusercontent.com/u/1585949?v=4&s=80",
     "josh" : "https://avatars3.githubusercontent.com/u/1072233?v=4&s=80",
-    "shash" : "https://avatars3.githubusercontent.com/u/9938926?v=4&s=80",
     "nader" : "https://avatars3.githubusercontent.com/u/8159867?v=4&s=80",
-    "jamie" : "https://avatars3.githubusercontent.com/u/22056320?v=4&s=80"
+    "jamie" : "https://avatars3.githubusercontent.com/u/22056320?v=4&s=80",
+    "divya" : "https://avatars2.githubusercontent.com/u/36862811?s=400&v=4"
   },
   "badges": {},
   "locked": {},
